d47a343ed832fd6f39db3ebcc6c3730e33d05cea
Chuck Tilbury <chucktilbury@gmail.com>
2024-04-27 13:17:14 -0500

    Use templates for the tokens source code.

--- src/tokens/mk_tokens.py
+++ src/tokens/mk_tokens.py
@@ -1,165 +1,31 @@
-# Read the keywords.txt file and generate keywords.c and keywords.h
 import sys
 
-raw_list = []
-with open(sys.argv[1], "r") as inf:
-    for line in inf:
-        s = line.strip()
-        if len(s) > 0:
-            raw_list.append(s)
+#with open("keywords.txt", "r") as fh:
+with open(sys.argv[1], "r") as fh:
+    word_list = fh.readlines()
 
-raw_list.sort()
-#print (raw_list)
+#with open("tokens.h.template", "r") as fh:
+with open(sys.argv[2], "r") as fh:
+    header = fh.read()
 
-with open("tokens.h", "w") as outf:
+#with open("tokens.c.template", "r") as fh:
+with open(sys.argv[3], "r") as fh:
+    source = fh.read()
 
-    outf.write('''
-/* This file is generated with a script. Do not edit. */
-#ifndef _TOKENS_H
-#define _TOKENS_H
-//#include "util.h"
-#include <stdbool.h>
-#include "sstrings.h"
+toklist = sorted(["TOK_%s"%(w.strip().upper()) for w in word_list])
 
-typedef enum {
-    // markers
-    TOK_END_OF_FILE,
-    TOK_END_OF_INPUT,
-    TOK_ERROR,\n
-    // keywords
-''')
+hout = header%(
+    "\n".join(["    %s,"%(w) for w in toklist]))
 
-    for item in raw_list:
-        outf.write("    TOK_%s,\n"%(item.upper()))
+with open("tokens.h", "w") as fh:
+    fh.write(hout)
 
-    outf.write('''
+sec1 = "\n".join("    { \"%s\", TOK_%s },"%(
+            w.strip(), w.strip().upper()) for w in sorted(word_list))
+sec2 = "\n".join("        (tok->type == TOK_%s)? \"%s\" :"%(
+            w.strip().upper(), w.strip()) for w in word_list)
 
-    // operators
-    TOK_LORE,
-    TOK_GORE,
-    TOK_EQU,
-    TOK_NEQU,
-    TOK_ADD_ASSIGN,
-    TOK_SUB_ASSIGN,
-    TOK_MUL_ASSIGN,
-    TOK_DIV_ASSIGN,
-    TOK_MOD_ASSIGN,
-    TOK_ADD,
-    TOK_SUB,
-    TOK_ASSIGN,
-    TOK_DIV,
-    TOK_MUL,
-    TOK_MOD,
-    TOK_OPAREN,
-    TOK_CPAREN,
-    TOK_OCBRACE,
-    TOK_CCBRACE,
-    TOK_OSBRACE,
-    TOK_CSBRACE,
-    TOK_COMMA,
-    TOK_DOT,
-    TOK_OPBRACE,
-    TOK_CPBRACE,
-    TOK_COLON,
-    TOK_CARAT,
-    TOK_AMPER,
+cout = source%(sec1, sec2)
 
-    // constructed tokens
-    TOK_LITERAL_FLOAT,
-    TOK_LITERAL_SIGNED,
-    TOK_LITERAL_UNSIGNED,
-    TOK_LITERAL_DSTR,
-    TOK_LITERAL_SSTR,
-    TOK_SYMBOL,
-    TOK_UNARY_MINUS,
-} TokenType;
-
-/**
- * @brief The parser expects a token to look like this.
- */
-typedef struct {
-    const char* str;
-    TokenType type;
-} KeywordList;
-
-/**
- * @brief The parser expects a token to look like this.
- */
-typedef struct {
-    String* str;       // String that caused the token to be recognized
-    TokenType type;    // Type of the token
-    int line_no;       // Line number where the token was recognized
-    int col_no;        // Column of the last character of the token
-    const char* fname; // File name where the token was taken
-} Token;
-
-extern KeywordList keyword_list[];
-extern const int num_keywords;
-
-const char* tok_to_str(Token* tok);
-
-''')
-
-    outf.write("#endif /* _TOKENS_H */\n\n")
-
-with open("tokens.c", "w") as outf:
-    outf.write("/* This file is generated with a script. Do not edit. */\n\n")
-    outf.write("#include <assert.h>\n\n")
-    outf.write("#include \"tokens.h\"\n\n")
-    outf.write("KeywordList keyword_list[] = {\n")
-    for item in raw_list:
-        outf.write("    { \"%s\", TOK_%s },\n"%(item, item.upper()))
-    outf.write("};\n\n")
-    outf.write("const int num_keywords = (sizeof(keyword_list) / sizeof(KeywordList));\n\n")
-
-    outf.write("const char* tok_to_str(Token* tok) {\n")
-    outf.write("    assert(tok != NULL);\n\n")
-    outf.write("    return\n        /* keywords */\n")
-    for item in raw_list:
-        outf.write("        (tok->type == TOK_%s)? \"%s\":\n"%(item.upper(), item))
-
-    outf.write('''
-        /* other tokens */
-        (tok->type == TOK_END_OF_FILE)? "end of file":
-        (tok->type == TOK_END_OF_INPUT)? "end of input":
-        (tok->type == TOK_ERROR)? "error":
-        (tok->type == TOK_LORE)? "<=":
-        (tok->type == TOK_GORE)? ">=":
-        (tok->type == TOK_EQU)? "==":
-        (tok->type == TOK_NEQU)? "!=":
-        (tok->type == TOK_OR)? "or":
-        (tok->type == TOK_AND)? "and":
-        (tok->type == TOK_NOT)? "not":
-        (tok->type == TOK_ADD_ASSIGN)? "+=":
-        (tok->type == TOK_SUB_ASSIGN)? "-=":
-        (tok->type == TOK_MUL_ASSIGN)? "*=":
-        (tok->type == TOK_DIV_ASSIGN)? "/=":
-        (tok->type == TOK_MOD_ASSIGN)? "%=":
-        (tok->type == TOK_ADD)? "+":
-        (tok->type == TOK_SUB)? "-":
-        (tok->type == TOK_ASSIGN)? "=":
-        (tok->type == TOK_DIV)? "/":
-        (tok->type == TOK_MUL)? "*":
-        (tok->type == TOK_MOD)? "%":
-        (tok->type == TOK_OPAREN)? "(":
-        (tok->type == TOK_CPAREN)? ")":
-        (tok->type == TOK_OCBRACE)? "{":
-        (tok->type == TOK_CCBRACE)? "}":
-        (tok->type == TOK_OSBRACE)? "[":
-        (tok->type == TOK_CSBRACE)? "]":
-        (tok->type == TOK_COMMA)? ",":
-        (tok->type == TOK_DOT)? ".":
-        (tok->type == TOK_OPBRACE)? "<":
-        (tok->type == TOK_CPBRACE)? ">":
-        (tok->type == TOK_COLON)? ":":
-        (tok->type == TOK_CARAT)? "^":
-        (tok->type == TOK_AMPER)? "&":
-        (tok->type == TOK_LITERAL_FLOAT)? "literal float":
-        (tok->type == TOK_LITERAL_SIGNED)? "literal signed":
-        (tok->type == TOK_LITERAL_UNSIGNED)? "literal unsigned":
-        (tok->type == TOK_LITERAL_DSTR)? "literal dstrg":
-        (tok->type == TOK_LITERAL_SSTR)? "literal sstrg":
-        (tok->type == TOK_SYMBOL)? "symbol":
-        (tok->type == TOK_UNARY_MINUS)? "unary minus": "UNKNOWN";\n}\n\n
-''')
-
+with open("tokens.c", "w") as fh:
+    fh.write(cout)
